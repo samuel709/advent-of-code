4e290a7e42ececb41d5b26247a8464e1cda08f5b
Samuel <samuelurquhart96@gmail.com>
2021-02-01 16:56:41 -0700

    works for a single input

--- day-2/program.py
+++ day-2/program.py
@@ -2,6 +2,8 @@
 1-3 x: xijijppjwqx
 Numbers indicate the number of times x must be present in the string
 In this case, x must be present at least once and no more than three times
+
+Input is always assumed to be structured identically to ^^^
 """
 
 
@@ -16,24 +18,37 @@ In this case, x must be present at least once and no more than three times
     # print(words_list)
     
     # file.close()
-file = open("short-text.txt")
-
-my_list = []
-
-my_word = file.readline().split()
-num = my_word[0].split('-')
-for x in num:
-    my_list.append(int(x))
     
-print(type(my_list[0]))    
-print(num)
-print(my_word)
+file = open("short-text.txt")
+is_valid = False
+
+###Splits password into an array
+###[upper and lower limits, character we are counting, string we are to check]
+password = file.readline().split()
+
+###Get the lower and upper limits from our password 
+nums = password[0].split('-')
+lower_limit = int(nums[0])
+upper_limit = int(nums[1])
+
+###Get the character we are counting in our string
+char_to_check = password[1][0]
+
+###Count the number of times a character appears in the string
+###Return True if count is greater than lower limit and less than upper limit
+letter_count = password[2].count(char_to_check)
+if letter_count > lower_limit and letter_count < upper_limit:
+    is_valid = True
+
+
+for x in file:
+    print(x)
+
+    
+# print(lower_limit, upper_limit)
+# print(char_to_check)
+# print(letter_count)
+# print(is_valid)
 
 file.close()
 
-# nums = my_array[0].split('-')
-# for x in nums:
-    
-# print(nums)   
-
-# text_to_array("short-text.txt")
