b01faf89f3fbd6a3a3bbb492391f85994fa77e63
Samuel <samuelurquhart96@gmail.com>
2021-02-03 15:18:02 -0700

    complete day 2

--- day-2/program.py
+++ day-2/program.py
@@ -1,54 +1,69 @@
 """
-1-3 x: xijijppjwqx
-Numbers indicate the number of times x must be present in the string
-In this case, x must be present at least once and no more than three times
+2-5 x: xijixppjwqx
+All input from text.txt is assumed to be structured identically to the one above.
 
-Input is always assumed to be structured identically to ^^^
+PART 1:
+Numbers indicate the number of times x must be present in the string.
+In this case, x must be present at least twice and no more than five times.
+
+PART 2:
+Numbers indicate two positions in the string, and our char must occupy ONLY one of those positions
+for the password to be valid.
+In this case, x must either be in the second or fifth position, but it can't be in both. 
+Their fictional password system is not zero-indexed.
 """
 
-
-# def text_to_array(filename):
-
-    # words_list = []
-    # file = open(filename)
-    
-    # for x in file:
-        # words_list.append(x.split())
-     
-    # print(words_list)
-    
-    # file.close()
-    
-file = open("short-text.txt")
-is_valid = False
-
-###Splits password into an array
-###[upper and lower limits, character we are counting, string we are to check]
-password = file.readline().split()
-
-###Get the lower and upper limits from our password 
-nums = password[0].split('-')
-lower_limit = int(nums[0])
-upper_limit = int(nums[1])
-
-###Get the character we are counting in our string
-char_to_check = password[1][0]
-
-###Count the number of times a character appears in the string
-###Return True if count is greater than lower limit and less than upper limit
-letter_count = password[2].count(char_to_check)
-if letter_count > lower_limit and letter_count < upper_limit:
-    is_valid = True
-
+###SOLUTION TO PART 1###
+"""
+file = open("text.txt")
+good_password_count = 0
 
 for x in file:
-    print(x)
-
     
-# print(lower_limit, upper_limit)
-# print(char_to_check)
-# print(letter_count)
-# print(is_valid)
+    ###Splits password into array ['1-3', 'a:', 'asdgasg']
+    pw_info = x.split()
+    
+    limits = pw_info[0].split('-')
+    lower_limit = int(limits[0])
+    upper_limit = int(limits[1])
+    
+    ###pw_char is the first character of the second element of our pw_info array
+    pw_char = pw_info[1][0]
+    
+    pw_string = pw_info[2]
+    letter_count = pw_string.count(pw_char)
+    
+    if letter_count >= lower_limit and letter_count <= upper_limit:
+        good_password_count += 1
+
+print(good_password_count)
 
 file.close()
+"""
 
+###SOLUTION TO PART 2###
+###Most of the logic here is copied from solution 1
+"""
+file = open("text.txt")
+good_password_count = 0
+
+for x in file:
+
+    pw_info = x.split()
+    
+    positions = pw_info[0].split('-')
+    first_pos = int(positions[0])
+    second_pos = int(positions[1])
+    
+    pw_char = pw_info[1][0]
+    pw_string = pw_info[2]
+    
+    ###Because their fictional password system is not zero-indexed we subtract one from each position
+    if pw_string[first_pos - 1] == pw_char and pw_string[second_pos - 1] != pw_char:
+        good_password_count += 1
+    if pw_string[second_pos -1] == pw_char and pw_string[first_pos - 1] != pw_char:
+        good_password_count += 1
+
+print(good_password_count)
+file.close()
+"""
